504d597569507ea3de13729f240d9e4db6a24899
Gabriel Latendresse <glatendresse@localhost.localdomain>
2017-03-15 19:33:37 -0400

    package.json changes

--- config/lib/app.js
+++ config/lib/app.js
@@ -34,9 +34,9 @@ module.exports.start = function start(callback) {
   _this.init(function (app, db, config) {
 
     // Start the app by listening on <port> at <host>
-    app.listen(process.env.PORT, '0.0.0.0', function () {
+    app.listen(config.port, config.host, function () {
       // Create server URL
-      var server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + '0.0.0.0' + ':' + process.env.PORT;
+      var server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
       // Logging initialization
       console.log('--');
       console.log(chalk.green(config.app.title));
